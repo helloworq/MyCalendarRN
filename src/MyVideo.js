6840d44c12cf2c31f3e5d43b342abcbc9c9887b9
lei.zhou <lei.zhou@derbysoft.net>
2023-08-30 22:26:11 +0800

    支持下载文件

--- src/MyVideo.js
+++ src/MyVideo.js
@@ -11,31 +11,30 @@ import ImgStroage from "./storage/ImgStroage";
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import Video from 'react-native-video';
 import { PreferencesContext } from "./MyPreferencesContext";
-// At the top where our imports are...
 import VideoPlayer from 'react-native-media-console';
 import Orientation from 'react-native-orientation-locker';
 import RNFS from 'react-native-fs'
 import { FFmpegKit } from 'ffmpeg-kit-react-native';
-import { LogLevel, RNFFmpeg } from 'react-native-ffmpeg';
+import { ProgressBar, MD3Colors } from 'react-native-paper';
+
 
 const MyVideo = () => {
     const portraitHeight = 300
     const fullScreenHeight = '100%'
     const [isFullscreen, setIsFullScreen] = useState(false)
     const videopath = '/千万别模仿.flv'
+    const [progress, setProgress] = useState(0)
 
 
     return (
         <>
-            <Button title='Click Me' onPress={() => {
+            <Button title='视频转换' onPress={() => {
                 const path = RNFS.ExternalDirectoryPath
                 console.log(path)
 
                 FFmpegKit.execute(` -i ${path + videopath} -y -vcodec copy -acodec copy ${path + '/output.mp4'}`)
-
             }} />
             <View style={{ flex: 1, }}>
-                <Button title='11' />
                 <VideoPlayer
                     source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
                     //isFullscreen={true}
@@ -56,13 +55,42 @@ const MyVideo = () => {
                     containerStyle={{
                         width: '100%',
                         height: isFullscreen ? fullScreenHeight : portraitHeight,
-                        position: 'absolute',
-                        left: 0,
-                        right: 0,
-                        top: 0,
-                        down: 0,
+                        //position: 'absolute',
+                        //left: 0,
+                        //right: 0,
+                        //top: 0,
+                        //down: 0,
                     }}
                 />
+
+                <ProgressBar progress={progress} color={MD3Colors.error50} />
+                <Button title='Bilibili视频' onPress={() => {
+                    const path = RNFS.ExternalDirectoryPath
+                    RNFS.downloadFile({
+                        fromUrl: 'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4',
+                        toFile: path + '/test.mp4',
+                        background: true,
+                        begin: (res) => {
+                            console.log('begin', res);
+                            console.log('contentLength:', res.contentLength / 1024 / 1024, 'M');
+                        },
+                        progress: (res) => {
+                            let pro = res.bytesWritten / res.contentLength;
+                            setProgress(pro)
+                        }
+                    }).promise.then(e => {
+                        console.log('done', e)
+                        setProgress(1)
+                    }).catch(e=>console.log(e))
+
+
+                }} />
+                {/* <Button title='Bilibili视频' onPress={() => {
+                    const path = RNFS.ExternalDirectoryPath
+                    console.log(path)
+
+                    FFmpegKit.execute(` -i ${path + videopath} -y -vcodec copy -acodec copy ${path + '/output.mp4'}`)
+                }} /> */}
             </View>
         </>
     )
