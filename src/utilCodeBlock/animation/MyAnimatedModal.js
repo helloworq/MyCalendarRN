29a0c85e6b4f05c9cad82394454d4f2128d19d5a
lei.zhou <1273370082@qq.com>
2023-09-05 18:47:56 +0800

    调整样式

--- src/utilCodeBlock/animation/MyAnimatedModal.js
+++ src/utilCodeBlock/animation/MyAnimatedModal.js
@@ -22,76 +22,77 @@ const MyAnimatedModal = () => {
     return (
         <>
             <View style={{ backgroundColor: 'black', flex: 1 }}>
-                <View style={{ height: radius }} >
-                    <Animated.View
-                        style={{
-                            width: radius,
-                            height: height,
-                            borderRadius: radius / 2,
-                            opacity: height,
-                            position: 'absolute',
-                            backgroundColor: 'green',
-                        }}
-                    >
-                        <View style={{
-                            position: 'absolute',
-                            //left: 0, top: 0, right: 0, bottom: 0, alignItems: 'center',justifyContent: 'center',
-                            height: radius,
-                            alignSelf: 'center'
-                        }}>
-                            <TouchableOpacity onPress={() => {
-                                const doOpen = !open
-                                setOpen(doOpen)
-                                if (!doOpen) {
-                                    //如果是关闭组件，则立即去掉额外组件
-                                    setRestCompoment(<></>)
-                                }
-                                setTimeout(() => {
-                                    //默认组件一定加载成功，将处理事件放在这里而不是.start()回调事件
-                                    //加载完成事件判断是否需要设置剩余组件
-                                    setRestCompoment(
-                                        doOpen ? <>
-                                            <TouchableOpacity onPress={() => {
-                                                setRestCompoment(<></>)
-                                                setOpen(false)
-                                                Animated.spring(
-                                                    height,
-                                                    {
-                                                        toValue: radius,
-                                                        duration: animationDuration,
-                                                        useNativeDriver: false,
-                                                    },
-                                                ).start();
-                                            }} >
-                                                <MaterialCommunityIcons
-                                                    name='download'
-                                                    size={radius}
-                                                    color={'yellow'}
-                                                />
-                                            </TouchableOpacity>
-                                        </> : <></>
-                                    )
-                                }, animationDuration / 2)
-                                Animated.spring(
-                                    height,
-                                    {
-                                        toValue: (doOpen ? 2 * radius : radius),
-                                        duration: animationDuration,
-                                        useNativeDriver: false,
-                                    },
-                                ).start();
-                            }} >
-                                <AntDesign
-                                    name='plus'
-                                    size={radius}
-                                    color={'yellow'}
-                                />
-                                {restCompoment}
-                            </TouchableOpacity>
-                        </View>
-                    </Animated.View>
-                </View>
 
+
+                <Animated.View
+                    style={{
+                        width: radius,
+                        height: height,
+                        borderRadius: radius / 2,
+                        opacity: height,
+                        position: 'absolute',
+                        backgroundColor: 'green',
+                    }}
+                >
+                    <View style={{
+                        position: 'absolute',
+                        //left: 0, top: 0, right: 0, bottom: 0, alignItems: 'center',justifyContent: 'center',
+                        height: radius,
+                        alignSelf: 'center', 
+                    }}>
+                        <TouchableOpacity onPress={() => {
+                            const doOpen = !open
+                            setOpen(doOpen)
+                            if (!doOpen) {
+                                //如果是关闭组件，则立即去掉额外组件
+                                setRestCompoment(<></>)
+                            }
+                            setTimeout(() => {
+                                //默认组件一定加载成功，将处理事件放在这里而不是.start()回调事件
+                                //加载完成事件判断是否需要设置剩余组件
+                                setRestCompoment(
+                                    doOpen ? <>
+                                        <TouchableOpacity onPress={() => {
+                                            setRestCompoment(<></>)
+                                            setOpen(false)
+                                            Animated.spring(
+                                                height,
+                                                {
+                                                    toValue: radius,
+                                                    duration: animationDuration,
+                                                    useNativeDriver: false,
+                                                },
+                                            ).start();
+                                        }} >
+                                            <MaterialCommunityIcons
+                                                name='download'
+                                                size={radius}
+                                                color={'yellow'}
+                                            />
+                                        </TouchableOpacity>
+                                    </> : <></>
+                                )
+                            }, animationDuration / 2)
+                            Animated.spring(
+                                height,
+                                {
+                                    toValue: (doOpen ? 2 * radius : radius),
+                                    duration: animationDuration,
+                                    useNativeDriver: false,
+                                },
+                            ).start();
+                        }} >
+                            <AntDesign
+                                name='plus'
+                                size={radius}
+                                color={'yellow'}
+                            />
+                            {restCompoment}
+                        </TouchableOpacity>
+                    </View>
+                </Animated.View>
+
+                <Button title='111' />
             </View >
         </>
     );
