e5397f6ca44b7f667b656fb21a45427ef5695387
lei.zhou <1273370082@qq.com>
2023-09-19 13:31:47 +0800

    支持点击地图

--- src/utilCodeBlock/svg/MySvg.js
+++ src/utilCodeBlock/svg/MySvg.js
@@ -11,15 +11,12 @@ import {
 } from "react-native";
 import { Svg, SvgXml, Path, Circle, Rect, TextPath, TSpan } from 'react-native-svg';
 
-const MySvgMap = ({ id, path, func }) => {
-    const [x,setX] = useState(0)
-    const [y,setY] = useState(0)
-
+const MySvgMap = ({ id, path, func, select, setSelect }) => {
     return (<>
         <Path
             id={id}
-            fill="#eee"
-            fillOpacity={0.1}
+            fill={select === id ? "green" : "red"}
+            fillOpacity={0.5}
             fillRule={'evenodd'}
             stroke="#333"
             strokeOpacity={1}
@@ -28,19 +25,14 @@ const MySvgMap = ({ id, path, func }) => {
             strokeLinejoin={'miter'}
             strokeDasharray={1}
             d={path}
-            // onPress={(e) => console.log(id,e)}
-            onLayout={(e) => {
-                console.log('>>>>>', e.nativeEvent.layout)
-                setX(e.nativeEvent.layout.x)
-                setY(e.nativeEvent.layout.y)
-            }}
+            onPress={(e) => setSelect(id)}
         />
-        <Rect x={x+20} y={y+20} height="20" width="20" stroke="#060" fill="#060" />
     </>
     )
 }
 
 const MySvg = () => {
+    const [select, setSelect] = useState()
     const header = `<svg xmlns="http://www.w3.org/2000/svg" pointer-events="none" class="leaflet-zoom-animated" width="664" height="628" viewBox="-68 -52 664 628" style="transform: translate3d(-68px, -52px, 0px);"><g>`
     const footer = `</g></svg>`
     const anyuan = `M276 245L273 250L272 258L276 260L274 271L276 273L282 272L284 278L287 278L289 281L286 283L294 293L296 294L303 291L303 283L305 282L321 282L321 276L328 271L326 267L328 264L330 254L329 248L324 249L317 257L304 256L301 260L293 258L292 262L287 263L286 258L279 254L279 250L283 242L278 244z`
@@ -50,18 +42,6 @@ const MySvg = () => {
     const shangli = `M253 243L256 241L266 240L267 243L264 246L263 251L266 252L272 247L282 242L283 243L279 250L279 254L287 259L287 263L291 262L293 258L301 260L304 256L317 257L324 249L328 247L328 245L330 246L347 237L348 235L342 236L342 234L336 230L335 232L331 233L327 230L324 231L325 227L321 225L321 220L323 218L320 215L317 216L317 218L315 218L314 210L305 210L307 209L305 209L305 207L307 207L308 202L307 200L303 199L311 189L316 186L316 190L318 190L321 185L324 185L327 180L331 178L332 174L329 169L326 168L323 171L320 170L318 167L319 164L312 166L311 163L304 165L300 164L299 161L296 160L293 168L288 169L285 166L280 169L278 173L274 174L267 181L266 186L263 188L261 197L263 200L264 199L269 203L271 217L254 235L254 239z`
     const px = `M298 340L299 351L294 363L295 365L298 365L298 369L295 373L292 387L288 391L284 401L274 412L273 425L277 426L279 423L281 423L284 438L286 441L290 441L292 446L296 446L298 449L298 454L302 459L310 463L312 462L313 457L322 451L322 443L325 442L328 436L331 435L332 431L330 430L330 427L334 417L344 406L351 406L359 387L363 388L372 381L369 368L367 366L368 360L366 359L362 362L356 355L350 354L350 350L344 346L344 344L341 341L337 339L339 328L342 326L347 327L348 325L352 327L358 325L367 327L376 322L378 316L382 314L381 313L384 307L382 305L383 301L389 298L389 293L391 291L395 290L396 288L399 288L399 286L403 284L401 284L400 281L393 277L382 266L387 259L386 256L381 251L381 248L376 238L371 238L371 233L369 233L367 230L365 231L364 228L362 228L359 233L356 232L349 236L342 236L342 234L336 230L335 232L331 233L327 230L324 231L325 227L321 225L321 220L323 218L320 215L317 216L317 218L315 218L314 210L305 210L307 209L305 209L305 207L307 207L308 202L307 200L303 199L307 195L307 193L316 186L316 190L318 190L321 185L324 185L327 180L331 178L332 174L328 168L326 168L323 171L320 170L318 167L319 164L312 166L311 163L304 165L300 164L299 161L296 160L293 168L288 169L285 166L283 167L278 171L278 173L274 174L267 181L266 186L263 188L261 197L263 200L264 199L269 203L271 217L254 235L253 243L247 248L242 259L233 265L231 268L231 282L229 283L226 290L223 293L223 297L225 300L231 302L234 299L236 303L234 305L235 307L232 313L226 316L229 330L234 330L234 333L236 334L229 340L230 347L234 352L240 350L247 355L254 355L260 350L263 351L270 347L281 345L286 346L295 342z`
 
-    const flags = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" id="flag-icons-cn" viewBox="0 0 640 480">
-    <defs>
-      <path id="cn-a" fill="#ff0" d="M-.6.8 0-1 .6.8-1-.3h2z"/>
-    </defs>
-    <path fill="#ee1c25" d="M0 0h640v480H0z"/>
-    <use xlink:href="#cn-a" width="30" height="20" transform="matrix(71.9991 0 0 72 120 120)"/>
-    <use xlink:href="#cn-a" width="30" height="20" transform="matrix(-12.33562 -20.5871 20.58684 -12.33577 240.3 48)"/>
-    <use xlink:href="#cn-a" width="30" height="20" transform="matrix(-3.38573 -23.75998 23.75968 -3.38578 288 95.8)"/>
-    <use xlink:href="#cn-a" width="30" height="20" transform="matrix(6.5991 -23.0749 23.0746 6.59919 288 168)"/>
-    <use xlink:href="#cn-a" width="30" height="20" transform="matrix(14.9991 -18.73557 18.73533 14.99929 240 216)"/>
-  </svg>
-  `
     return (
         <>
 
@@ -70,12 +50,12 @@ const MySvg = () => {
                     width={600}
                     height={600}
                 >
-                    <MySvgMap id='安源区' path={anyuan} />
-                    <MySvgMap id='湘东区' path={xiangdong} />
-                    <MySvgMap id='上栗县' path={shangli} />
-                    <MySvgMap id='芦溪县' path={luxi} />
-                    <MySvgMap id='莲花县' path={lianhua} />
-                    {/* <MySvgMap id='萍乡市' path={px} /> */}
+                    <MySvgMap id='安源区' path={anyuan} select={select} setSelect={(e) => setSelect(e)} />
+                    <MySvgMap id='湘东区' path={xiangdong} select={select} setSelect={(e) => setSelect(e)} />
+                    <MySvgMap id='上栗县' path={shangli} select={select} setSelect={(e) => setSelect(e)} />
+                    <MySvgMap id='芦溪县' path={luxi} select={select} setSelect={(e) => setSelect(e)} />
+                    <MySvgMap id='莲花县' path={lianhua} select={select} setSelect={(e) => setSelect(e)} />
+                    {/* <MySvgMap id='萍乡市' path={px} select={select} setSelect={(e) => setSelect(e)} /> */}
                 </Svg>
             </View>
         </>
