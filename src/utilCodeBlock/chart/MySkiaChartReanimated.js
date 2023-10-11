5447d806a19a23d16879b2e8591ab776406ae8ee
lei.zhou <lei.zhou@derbysoft.net>
2023-10-11 21:34:36 +0800

    支持折线小点显示

--- src/utilCodeBlock/chart/MySkiaChartReanimated.js
+++ src/utilCodeBlock/chart/MySkiaChartReanimated.js
@@ -7,56 +7,129 @@ import Animated, {
     useSharedValue,
     withTiming,
 } from "react-native-reanimated";
+import {
+    Gesture,
+    GestureDetector,
+    GestureHandlerRootView,
+} from 'react-native-gesture-handler';
 import Svg, {
     Line,
+    Text,
+    TSpan,
+    Circle,
 } from 'react-native-svg';
 
 
-const AnimatedLine = Animated.createAnimatedComponent(Line)
+
 const AnimatedLineEle = ({ px1, py1, px2, py2 }) => {
+    const AnimatedLine = Animated.createAnimatedComponent(Line)
+    const duration = 500
     const x1 = useSharedValue(0)
     const y1 = useSharedValue(0)
     const x2 = useSharedValue(0)
     const y2 = useSharedValue(0)
 
     useEffect(() => {
-        console.log(px1, py1, px2, py2, x1, y1, x2, y2)
-        x1.value = withTiming(px1, { duration: 1000 }),
-            y1.value = withTiming(py1, { duration: 2000 }),
-            x2.value = withTiming(px2, { duration: 2000 }),
-            y2.value = withTiming(py2, { duration: 2000 })
+        x1.value = withTiming(px1, { duration: duration })
+        y1.value = withTiming(py1, { duration: duration })
+        x2.value = withTiming(px2, { duration: duration })
+        y2.value = withTiming(py2, { duration: duration })
     }, [px1, py1, px2, py2])
 
     return (
         <>
-            <AnimatedLine x1={x1} y1={y1} x2={x2} y2={y2} stroke="red" strokeWidth="2" />
+            <AnimatedLine x1={x1} y1={y1} x2={x2} y2={y2} stroke="skyblue" strokeWidth="2" />
         </>
     )
 }
 
+
 const MySkiaChartReanimated = () => {
+    const size = 31
+    const poolSize = 10
+    const margin = 20
+    const maxHeight = 100
     const screenWidth = Dimensions.get("window").width
-    const split = screenWidth / 31
+    const split = screenWidth / size
     const [press, setPress] = useState(true)
     const [block, setBlock] = useState()
-    const first = [95, 7, 146, 37, 126, 53, 85, 25, 79, 167, 171, 130, 40, 151, 84, 172, 136, 32, 101, 143, 17, 15, 166, 72, 43, 134, 179, 61, 49, 129, 154]
-    const second = [138, 13, 69, 144, 129, 71, 79, 136, 114, 91, 118, 173, 145, 51, 36, 95, 47, 117, 145, 150, 161, 82, 178, 136, 164, 168, 33, 184, 182, 142, 119]
+    const [xAsix, setXAsix] = useState()
+    const offsetX = useSharedValue(0)
+    const offsetY = useSharedValue(0)
+    const [points, setPoints] = useState({})
+    const curIndex = useSharedValue(0)
+    const pool = useSharedValue(size)
+    const AnimatedCircle = Animated.createAnimatedComponent(Circle)
 
+    const pan = Gesture.Pan()
+        .onChange((event) => {
+            if (pool.value === poolSize) {
+                pool.value = pool.value - 1
+                if (event.changeX > 0) {
+                    curIndex.value = (curIndex.value > 25 ? 25 : curIndex.value) + 1
+                    offsetX.value = points[curIndex.value]['x']
+                    offsetY.value = points[curIndex.value]['y']
+                } else {
+                    curIndex.value = (curIndex.value < 3 ? 3 : curIndex.value) - 1
+                    offsetX.value = points[curIndex.value]['x']
+                    offsetY.value = points[curIndex.value]['y']
+                }
+            } else {
+                pool.value = pool.value === 0 ? poolSize : (pool.value - 1)
+            }
+        })
 
     return (<>
-        <Svg height="400" width="600">
-            {block}
-        </Svg>
-        <Button title='click' onPress={() => {
+        <GestureHandlerRootView style={{
+            alignItems: 'center',
+            justifyContent: 'center',
+        }}>
+            <GestureDetector gesture={pan}>
+                <Svg height={maxHeight + margin * 3} width={screenWidth}>
+                    {block}
+                    <Text y={maxHeight + margin * 2} fontSize={8}>
+                        {xAsix}
+                    </Text>
+                    <AnimatedCircle
+                        cx={offsetX}
+                        cy={offsetY}
+                        r="5"
+                        strokeWidth="2.5"
+                        fill="skyblue"
+                    />
+                </Svg>
+            </GestureDetector>
+        </GestureHandlerRootView >
+
+        <Button title='初始化' onPress={() => {
             let b = []
-            const arr = press ? first : second
-            for (let i = 0; i < arr.length - 1; i++) {
-                const cur = arr[i]
-                const later = arr[i + 1]
-                b.push(<AnimatedLineEle px1={i * split} py1={cur} px2={(i + 1) * split} py2={later} />)
+            let xA = []
+
+            let data = []
+            let pointData = {}
+            for (let i = 0; i < size; i++) {
+                const randomValue = Math.random() * maxHeight + margin
+                const temp = { x: (i * split), y: randomValue }
+                pointData[i] = temp
+                data.push(randomValue)
+            }
+            setPoints(pointData)
+
+            for (let i = 1; i < data.length; i++) {
+                const cur = data[i]
+                const later = data[i + 1]
+                if (i < (size - 1)) {
+                    b.push(<AnimatedLineEle px1={i * split} py1={cur} px2={(i + 1) * split} py2={later} />)
+                }
+                xA.push(
+                    <TSpan x={i * split}>
+                        {i}
+                    </TSpan>
+                )
             }
             setBlock(b)
             setPress(!press)
+            setXAsix(xA)
         }} />
     </>
     );
